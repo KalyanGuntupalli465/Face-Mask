4146915cee47073afee7d3ae23d4d1fd250be106
gmk <gmk@example.com>
2024-04-13 13:07:30 +0530

    initial commit

---
+++ App.py
@@ -0,0 +1,108 @@
+import streamlit as st
+from PIL import Image, ImageEnhance
+import numpy as np
+import os
+import cv2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import imutils
+from detect_mask_video import Video
+st.set_page_config(page_title='Face Mask Detector', page_icon='😷', layout='centered', initial_sidebar_state='expanded')
+
+
+
+def mask_detection():
+    st.markdown('<h1 align="center">😷 Face Mask Detection</h1>', unsafe_allow_html=True)
+    activities = ["Image", "Webcam"]
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+    st.sidebar.markdown("# Mask Detection on?")
+    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+
+    if choice == 'Image':
+        RG_img=None
+        st.markdown('<h2 align="center">Detection on Image</h2>', unsafe_allow_html=True)
+        st.markdown("### Upload your image here ⬇")
+        image_file = st.file_uploader("", type=['jpg'])  # upload image
+        if image_file is not None:
+            our_image = Image.open(image_file)  # making compatible to PIL
+            st.image(our_image, caption='', use_column_width=True)
+            st.markdown('<h3 align="center">Image uploaded successfully!</h3>', unsafe_allow_html=True)
+            if st.button('Process'):
+                print("[INFO] loading face detector model...")
+                prototxtPath = os.path.sep.join(["face_detector", "deploy.prototxt"])
+                weightsPath = os.path.sep.join(["face_detector",
+                                    "res10_300x300_ssd_iter_140000.caffemodel"])
+                net = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+         # load the face mask detector model from disk
+                print("[INFO] loading face mask detector model...")
+                model = load_model("mask_detector.model")
+
+    # load the input image from disk and grab the image spatial
+    # dimensions
+                image = np.array(our_image)
+                (h, w) = image.shape[:2]
+
+    # construct a blob from the image
+                blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
+                                 (104.0, 177.0, 123.0))
+
+    # pass the blob through the network and obtain the face detections
+                print("[INFO] computing face detections...")
+                net.setInput(blob)
+                detections = net.forward()
+        
+
+    # loop over the detections
+                for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+                    confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+                    if confidence > 0.5:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+                        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                        (startX, startY, endX, endY) = box.astype("int")
+
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+                        (startX, startY) = (max(0, startX), max(0, startY))
+                        (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # extract the face ROI, convert it from BGR to RGB channel
+            # ordering, resize it to 224x224, and preprocess it
+                        face = image[startY:endY, startX:endX]
+                        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                        face = cv2.resize(face, (224, 224))
+                        face = img_to_array(face)
+                        face = preprocess_input(face)
+                        face = np.expand_dims(face, axis=0)
+
+            # pass the face through the model to determine if the face
+            # has a mask or not
+                        (mask, withoutMask) = model.predict(face)[0]
+
+            # determine the class label and color we'll use to draw
+            # the bounding box and text
+                        label = "Mask" if mask > withoutMask else "No Mask"
+                        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+            # include the probability in the label
+                        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)   
+                        cv2.putText(image, label, (startX, startY - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                        cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+                        RG_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        if RG_img is not None:
+            st.image(RG_img, use_column_width=True)
+
+    if choice == 'Webcam':
+        st.markdown('<h2 align="center">Detection on Webcam</h2>', unsafe_allow_html=True)
+        st.write("Press Q to stop!!! :wink:")
+        Video()
+        
+mask_detection()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Face-Mask
+
+1) Clone Repo
+2) Install Requirements using pip install -r requirements.txt
+3) Run streamlit run App.py 
